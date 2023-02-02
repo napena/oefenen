0e7817524ddb975f0785636d725e2dcaaff2cbe7
Cxxxxxxx <firooww.dh@gmail.com>
2023-02-02 01:10:16 +0100

    Update buttons admin navbar

---
+++ src/Entity/Category.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CategoryRepository::class)]
+class Category
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $code = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $omschrijving = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getOmschrijving(): ?string
+    {
+        return $this->omschrijving;
+    }
+
+    public function setOmschrijving(string $omschrijving): self
+    {
+        $this->omschrijving = $omschrijving;
+
+        return $this;
+    }
+}
